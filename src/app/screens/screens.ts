7cfb9baffbc71c89f1de8a954d42fea6a613239f
cadub <cadubas1@gmail.com>
2025-09-20 21:03:54 -0300

    feat: adiciona novas telas para o objeto de Rotas

--- src/app/screens/screens.ts
+++ src/app/screens/screens.ts
@@ -7,11 +7,15 @@ import ProfileScreen from "./ProfileScreen"
 import OrganizationsScreen from "./OrganizationScreen";
 import OrganizationDetailScreen from "./OrganizationDetailScreen";
 import ScanScreen from "./ScanScreen";
+import QRCodeReaderScreen from "./QRCodeScanScreen";
+import AssetDetailScreen from "./AssetDetailScreen";
+import AttributeDetailScreen from "./AttributeDetailScreen";
 
 const SCREENS = { 
     HomeScreen, StorageScanScreen, DeviceDiscovery, 
     LoginScreen, ResultsScreen, ProfileScreen, OrganizationsScreen,
-    OrganizationDetailScreen, ScanScreen
+    OrganizationDetailScreen, ScanScreen, QRCodeReaderScreen, 
+    AssetDetailScreen, AttributeDetailScreen
 }
 
 export default SCREENS
